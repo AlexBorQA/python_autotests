c03b641d0a017ac051f508fab9fb4c7ca316cc07
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2022-12-03 16:19:33 +0500

    Delete test_pokemon/test_pan directory
    Корректировка названия

--- test_pokemon/test_pan/test_plan.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pytest
-def test_status_code():
-    response = requests.get('https://pokemonbattle.me:5000/trainers')
-    assert response.status_code == 200
-def test_piece_body():
-    response = requests.get('https://pokemonbattle.me:5000/trainers', params= {'trener_id': '1180'})  
-    assert response.json()['trainer_name'] = 'Alex'

b15f15addf33dbd72e649a0124dcaa37ccba4efd
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2022-12-04 19:38:15 +0500

    Update test_plan.py
    Корректировка описки

--- test_plan/test_plan.py
+++ test_plan/test_plan.py
@@ -4,5 +4,5 @@ def test_status_code():
     response = requests.get('https://pokemonbattle.me:5000/trainers')
     assert response.status_code == 200
 def test_piece_body():
-    response = requests.get('https://pokemonbattle.me:5000/trainers', params= {'trener_id': '1180'})  
-    assert response.json()['trainer_name'] = 'Alex'
+    response = requests.get('https://pokemonbattle.me:5000/trainers', params= {'trainer_id': '1180'})  
+    assert response.json()['trainer_name'] == 'Alex'
